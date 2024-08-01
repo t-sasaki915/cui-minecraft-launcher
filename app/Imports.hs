d391b1ab0ba6fcf4c7dd94f4275c11cc2bf5909e
Sasaki Touma <netst915@gmail.com>
2024-08-01 21:07:26 +0900

    add imports

---
+++ app/Imports.hs
@@ -0,0 +1,40 @@
+module Imports
+    ( filterM
+    , foldM
+    , foldM_
+    , forM
+    , forM_
+    , unless
+    , void
+    , when
+    , unlessM
+    , whenJust
+    , whenJustM
+    , whenM
+    , lift
+    , (<&>)
+    , isJust
+    , HasCallStack
+    , doesDirectoryExist
+    , doesFileExist
+    , (</>)
+    , printf
+    , (=~)
+    , version
+    ) where
+
+import           Control.Monad                (filterM, foldM, foldM_, forM,
+                                               forM_, unless, void, when)
+import           Control.Monad.Extra          (unlessM, whenJust, whenJustM,
+                                               whenM)
+import           Control.Monad.Trans.Class    (lift)
+import           Data.Functor                 ((<&>))
+import           Data.Maybe                   (isJust)
+import           GHC.Stack                    (HasCallStack)
+import           System.Directory             (doesDirectoryExist,
+                                               doesFileExist)
+import           System.FilePath              ((</>))
+import           Text.Printf                  (printf)
+import           Text.Regex.Posix             ((=~))
+
+import           Paths_cui_minecraft_launcher (version)
