cc8fa699c6e8e2d5aafd9ee9758173d8fbbbb3bb
Sasaki Touma <netst915@gmail.com>
2024-08-03 13:47:31 +0900

    update versionmanifest.hs

--- src/Data/Minecraft/VersionManifest.hs
+++ src/Data/Minecraft/VersionManifest.hs
@@ -1,8 +1,24 @@
-module Data.Minecraft.VersionManifest (MCVersion(..), VersionManifest(..)) where
+{-# LANGUAGE CPP #-}
 
-import           Core.Data.Clock (Time)
+module Data.Minecraft.VersionManifest
+    ( MCVersionID
+    , LatestVersions (..)
+    , MCVersionType (..)
+    , MCVersion (..)
+    , VersionManifest (..)
+    , fetchVersionManifestFromMojang
+    , parseVersionManifest
+    ) where
+
+import           Control.Monad            (when)
+import           Core.Data.Clock          (Time)
 import           Data.Aeson
-import           Text.Printf     (printf)
+import           Data.ByteString          (pack)
+import           Data.ByteString.Internal (c2w)
+import           GHC.Stack                (HasCallStack)
+import           System.Exit              (ExitCode (..))
+import           System.Process
+import           Text.Printf              (printf)
 
 type MCVersionID = String
 
@@ -19,11 +35,13 @@ instance FromJSON LatestVersions where
             <*> (m .: "snapshot")
     parseJSON x = fail (printf "Invalid LatestVersions structure: %s" (show x))
 
-data MCVersionType = Release | Snapshot deriving Show
+data MCVersionType = Release | Snapshot | OldBeta | OldAlpha deriving Show
 
 instance FromJSON MCVersionType where
-    parseJSON (String "release")  = pure Release
-    parseJSON (String "snapshot") = pure Snapshot
+    parseJSON (String "release")   = pure Release
+    parseJSON (String "snapshot")  = pure Snapshot
+    parseJSON (String "old_beta")  = pure OldBeta
+    parseJSON (String "old_alpha") = pure OldAlpha
     parseJSON x = fail (printf "Invalid MCVersionType structure: %s" (show x))
 
 data MCVersion = MCVersion
@@ -57,3 +75,41 @@ instance FromJSON VersionManifest where
             <$> (m .: "latest")
             <*> (m .: "versions")
     parseJSON x = fail (printf "Invalid VersionManifest structure: %s" (show x))
+
+curlExecName :: String
+#ifdef mingw32_HOST_OS
+curlExecName = "curl.exe"
+#endif
+#ifdef linux_HOST_OS
+curlExecName = "curl"
+#endif
+#ifdef darwin_HOST_OS
+curlExecName = "curl"
+#endif
+
+fetchVersionManifestFromMojang :: HasCallStack => IO String
+fetchVersionManifestFromMojang = do
+    let
+        curlArgs =
+            [ "--fail"
+            , "--silent"
+            , "--show-error"
+            , "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+            ]
+
+    (exitCode, stdout, stderr) <- readCreateProcessWithExitCode
+        (proc curlExecName curlArgs) []
+
+    when (exitCode /= ExitSuccess) $
+        error (printf "Failed to fetch version_manifest.json from Mojang server:\n%s" stderr)
+
+    return stdout
+
+parseVersionManifest :: HasCallStack => String -> VersionManifest
+parseVersionManifest rawJson =
+    case eitherDecodeStrict' (pack $ map c2w rawJson) of
+        Right versionManifest ->
+            versionManifest
+
+        Left err ->
+            error (printf "Failed to parse version_manifest.json: %s" err)
