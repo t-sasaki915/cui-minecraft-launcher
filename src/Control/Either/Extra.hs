0c35c1afe67901fc1b636c8cafaea7ac0adabdc5
Sasaki Touma <netst915@gmail.com>
2024-08-03 23:04:33 +0900

    update throweither

--- src/Control/Either/Extra.hs
+++ src/Control/Either/Extra.hs
@@ -3,9 +3,7 @@ module Control.Either.Extra (throwEither, throwEitherM) where
 import           GHC.Stack (HasCallStack)
 
 throwEither :: HasCallStack => Either String a -> a
-throwEither = \case
-    Right x     -> x
-    Left errMsg -> error errMsg
+throwEither = either error id
 
 throwEitherM :: (HasCallStack, Monad m) => m (Either String a) -> m a
 throwEitherM = (throwEither <$>)
