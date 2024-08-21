a3b53c9903fb285ece11d55b6bec1c440907673a
Sasaki Touma <netst915@gmail.com>
2024-08-21 19:16:11 +0900

    update system.process.extra

--- src/System/Process/Extra.hs
+++ src/System/Process/Extra.hs
@@ -3,21 +3,16 @@ module System.Process.Extra
     , execProcessEither
     ) where
 
+import           Data.Functor   ((<&>))
 import           System.Exit    (ExitCode (ExitSuccess))
 import           System.Process (proc, readCreateProcessWithExitCode)
 
 readProcessEither :: FilePath -> [String] -> IO (Either String String)
-readProcessEither execName execArgs = do
-    (exitCode, stdout, stderr) <- readCreateProcessWithExitCode (proc execName execArgs) []
-
-    case exitCode of
-        ExitSuccess -> return (Right stdout)
-        _           -> return (Left stderr)
+readProcessEither execName execArgs =
+    readCreateProcessWithExitCode (proc execName execArgs) [] <&> \case
+        (ExitSuccess, stdout, _) -> Right stdout
+        (_, _, stderr)           -> Left stderr
 
 execProcessEither :: FilePath -> [String] -> IO (Either String ())
-execProcessEither execName execArgs = do
-    (exitCode, _, stderr) <- readCreateProcessWithExitCode (proc execName execArgs) []
-
-    case exitCode of
-        ExitSuccess -> return (Right ())
-        _           -> return (Left stderr)
+execProcessEither execName execArgs =
+    readProcessEither execName execArgs <&> either Left (const (Right ()))
