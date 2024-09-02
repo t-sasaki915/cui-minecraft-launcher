05c66f6ff4d8d9d14f4e8e8b06de48d7be941667
Sasaki Touma <netst915@gmail.com>
2024-09-03 08:30:10 +0900

    arm64

--- src/System/OS/Arch.hs
+++ src/System/OS/Arch.hs
@@ -5,7 +5,7 @@ module System.OS.Arch (OSArch (..), currentOSArch) where
 import           Data.Aeson  (FromJSON (parseJSON), Value (String))
 import           Text.Printf (printf)
 
-data OSArch = X86 | X86_64 deriving (Show, Eq)
+data OSArch = X86 | X86_64 | Arm64 deriving (Show, Eq)
 
 currentOSArch :: OSArch
 #ifdef x86_64_HOST_ARCH
@@ -14,8 +14,12 @@ currentOSArch = X86_64
 #ifdef i386_HOST_ARCH
 currentOSArch = X86
 #endif
+#ifdef arm64_HOST_ARCH
+currentOSArch = Arm64
+#endif
 
 instance FromJSON OSArch where
     parseJSON (String "x86_64") = pure X86_64
     parseJSON (String "x86")    = pure X86
+    parseJSON (String "arm64")  = pure Arm64
     parseJSON x = fail (printf "Invalid OSArch structure: %s" (show x))
